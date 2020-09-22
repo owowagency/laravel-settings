befd9a9e2d93624c556a3c1763dbed4689ab4c53
albertpratomo <albertpratomo21@gmail.com>
2020-09-22 15:05:27 +0200

    Provide migration and config

--- src/LaravelNotificationsServiceProvider.php
+++ src/LaravelNotificationsServiceProvider.php
@@ -6,5 +6,27 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelNotificationsServiceProvider extends ServiceProvider
 {
-    //
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->registerPublishPaths();
+    }
+
+    /**
+     * Register paths to be published by the publish command.
+     *
+     * @return void
+     */
+    protected function registerPublishPaths(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/notifications.php' => config_path('notifications.php'),
+        ], 'notifications');
+    }
 }
